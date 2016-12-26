2045a5932b30ec7693a6a7e24bb02730476ba8a2
daycoder <daycoder@icloud.com>
2016-12-26 23:29:32 +0300

    asset list

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,8 @@ gulp.task('copy-js-vendors', function () {
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/clipboard/dist/clipboard.min.js',
     './node_modules/LykkeFramework/assets/js/vendor/fastclick.min.js',
-    './node_modules/LykkeFramework/assets/js/vendor/html5shiv.js'
+    './node_modules/LykkeFramework/assets/js/vendor/html5shiv.js',
+    './node_modules/in-view/dist/in-view.min.js'
     ])
     .pipe(gulp.dest('./public/js/vendor'));
 });
